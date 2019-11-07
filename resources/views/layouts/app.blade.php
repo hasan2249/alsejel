820f0be5cd23808ea1f1a8e0f9e39c86ab12a32b
Mohammad Nasser <mohammad.nasser@lit-co.net>
2019-11-07 13:34:49 +0200

    Add profile page template.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,6 +63,11 @@
 
                                 <ul class="dropdown-menu" role="menu">
                                     <li>
+                                        <a href="{{url('/user/'.Auth::user()->id)}}">
+                                            Profile
+                                        </a>
+                                    </li>
+                                    <li>
                                         <a href="{{ url('/logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,15 +1,192 @@
-
 @extends('home')
 
 @section('content2')
-<div>
-        <?php echo "....  Hi from <strong>User</strong> page"?>
-        
-        <h1>Name: {{$user->name}}</h1>
-        <h6>Id: {{$user->id}}</h6>
-        <h4>Created at: {{$user->created_at}}</h4>
-        <h5>Email: {{$user->email}}</h5>
-        <h2>Rule: <?php if($user->rule == 1) {echo"Admin";} else {echo"User";}?></h2>
+
+        <div class="container">
+                <div class="row my-2">
+                        <div class="col-lg-12 order-lg-2">
+
+                                <!--Navigation bar-->
+                                <ul class="nav nav-tabs">
+                                        <li class="nav-item">
+                                                <a href="" data-target="#Summery" data-toggle="tab" class="nav-link active">Summery</a>
+                                        </li>
+                                        <li class="nav-item">
+                                                <a href="" data-target="#Issues" data-toggle="tab" class="nav-link">Issues</a>
+                                        </li>
+                                        <li class="nav-item">
+                                                <a href="" data-target="#Activities" data-toggle="tab" class="nav-link">Activities</a>
+                                        </li>
+                                        <li class="nav-item">
+                                                <a href="" data-target="#edit" data-toggle="tab" class="nav-link">Edit</a>
+                                        </li>
+                                </ul>
+
+                                <!--Summery content-->
+                                <div class="tab-content py-4">
+                                        <div class="tab-pane active" id="Summery">
+                                                <h5 class="mb-3">User Profile</h5>
+                                                <div class="row">
+                                                        <div class="col-md-6">
+                                                                <h6>About TBD</h6>
+                                                                <p> Web Designer, UI/UX Engineer TBD </p>
+                                                        </div>
+                                                        <div class="col-md-12">
+                                                                <table class="table table-sm table-hover table-striped">
+                                                                        <tbody>
+                                                                        <tr>
+                                                                                <td>
+                                                                                        <strong>Name:</strong> {{$user->name}}
+                                                                                </td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                                <td>
+                                                                                        <strong>Email:</strong> {{$user->email}}
+                                                                                </td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                                <td>
+                                                                                        <strong>Created at:</strong> {{$user->created_at}}
+                                                                                </td>
+                                                                        </tr>
+                                                                        </tbody>
+                                                                </table>
+                                                        </div>
+                                                </div>
+                                        </div>
+                                        <div class="tab-pane" id="Activities">
+                                                <div class="alert alert-info alert-dismissable">
+                                                        <a class="panel-close close" data-dismiss="alert">×</a> This is an <strong>.alert</strong>. Use this to show important messages to the user. </div>
+                                                <table class="table table-hover table-striped">
+                                                        <tbody>
+                                                        <tr>
+                                                                <td>
+                                                                        <span class="float-right font-weight-bold">3 hrs ago</span> Here is your a link to the latest summary report from the.. </td>
+                                                        </tr>
+                                                        <tr>
+                                                                <td>
+                                                                        <span class="float-right font-weight-bold">Yesterday</span> There has been a request on your account since that was.. </td>
+                                                        </tr>
+                                                        <tr>
+                                                                <td>
+                                                                        <span class="float-right font-weight-bold">9/10</span> Porttitor vitae ultrices quis, dapibus id dolor. Morbi venenatis lacinia rhoncus. </td>
+                                                        </tr>
+                                                        <tr>
+                                                                <td>
+                                                                        <span class="float-right font-weight-bold">9/4</span> Vestibulum tincidunt ullamcorper eros eget luctus. </td>
+                                                        </tr>
+                                                        <tr>
+                                                                <td>
+                                                                        <span class="float-right font-weight-bold">9/4</span> Maxamillion ais the fix for tibulum tincidunt ullamcorper eros. </td>
+                                                        </tr>
+                                                        </tbody>
+                                                </table>
+                                        </div>
+                                        <div class="tab-pane" id="edit">
+                                                <form role="form">
+                                                        <div class="form-group row">
+                                                                <label class="col-lg-3 col-form-label form-control-label">First name</label>
+                                                                <div class="col-lg-9">
+                                                                        <input class="form-control" type="text" value="Jane">
+                                                                </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                                <label class="col-lg-3 col-form-label form-control-label">Last name</label>
+                                                                <div class="col-lg-9">
+                                                                        <input class="form-control" type="text" value="Bishop">
+                                                                </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                                <label class="col-lg-3 col-form-label form-control-label">Email</label>
+                                                                <div class="col-lg-9">
+                                                                        <input class="form-control" type="email" value="email@gmail.com">
+                                                                </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                                <label class="col-lg-3 col-form-label form-control-label">Company</label>
+                                                                <div class="col-lg-9">
+                                                                        <input class="form-control" type="text" value="">
+                                                                </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                                <label class="col-lg-3 col-form-label form-control-label">Website</label>
+                                                                <div class="col-lg-9">
+                                                                        <input class="form-control" type="url" value="">
+                                                                </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                                <label class="col-lg-3 col-form-label form-control-label">Address</label>
+                                                                <div class="col-lg-9">
+                                                                        <input class="form-control" type="text" value="" placeholder="Street">
+                                                                </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                                <label class="col-lg-3 col-form-label form-control-label"/>
+                                                                <div class="col-lg-6">
+                                                                        <input class="form-control" type="text" value="" placeholder="City">
+                                                                </div>
+                                                                <div class="col-lg-3">
+                                                                        <input class="form-control" type="text" value="" placeholder="State">
+                                                                </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                                <label class="col-lg-3 col-form-label form-control-label">Time Zone</label>
+                                                                <div class="col-lg-9">
+                                                                        <select id="user_time_zone" class="form-control" size="0">
+                                                                                <option value="Hawaii">(GMT-10:00) Hawaii</option>
+                                                                                <option value="Alaska">(GMT-09:00) Alaska</option>
+                                                                                <option value="Pacific Time (US &amp; Canada)">(GMT-08:00) Pacific Time (US &amp; Canada)</option>
+                                                                                <option value="Arizona">(GMT-07:00) Arizona</option>
+                                                                                <option value="Mountain Time (US &amp; Canada)">(GMT-07:00) Mountain Time (US &amp; Canada)</option>
+                                                                                <option value="Central Time (US &amp; Canada)" selected="selected">(GMT-06:00) Central Time (US &amp; Canada)</option>
+                                                                                <option value="Eastern Time (US &amp; Canada)">(GMT-05:00) Eastern Time (US &amp; Canada)</option>
+                                                                                <option value="Indiana (East)">(GMT-05:00) Indiana (East)</option>
+                                                                        </select>
+                                                                </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                                <label class="col-lg-3 col-form-label form-control-label">Username</label>
+                                                                <div class="col-lg-9">
+                                                                        <input class="form-control" type="text" value="janeuser">
+                                                                </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                                <label class="col-lg-3 col-form-label form-control-label">Password</label>
+                                                                <div class="col-lg-9">
+                                                                        <input class="form-control" type="password" value="11111122333">
+                                                                </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                                <label class="col-lg-3 col-form-label form-control-label">Confirm password</label>
+                                                                <div class="col-lg-9">
+                                                                        <input class="form-control" type="password" value="11111122333">
+                                                                </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                                <label class="col-lg-3 col-form-label form-control-label"/>
+                                                                <div class="col-lg-9">
+                                                                        <input type="reset" class="btn btn-secondary" value="Cancel">
+                                                                        <input type="button" class="btn btn-primary" value="Save Changes">
+                                                                </div>
+                                                        </div>
+                                                </form>
+                                        </div>
+                                </div>
+                        </div>
+                </div>
         </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
 @endsection
 
