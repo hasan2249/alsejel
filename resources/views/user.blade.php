cfe503ab460b7f86b8321dfdd0c1158b01d63fb0
Mohammad Nasser <mohammad.nasser@lit-co.net>
2019-11-11 13:07:03 +0200

    start working on profile page.

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -8,8 +8,6 @@
         <h1>Name: {{$user->name}}</h1><br/>
         <h1>Id: {{$user->id}}</h1><br/>
         <h1>Created at: {{$user->created_at}}</h1><br/>
-        <h1>Email: {{$user->email}}</h1><br/>
-        <h1>Rule: <?php if($user->rule == 1) {echo"Admin";} else {echo"User";}?></h1><br/>
         </div>
 @endsection
 
