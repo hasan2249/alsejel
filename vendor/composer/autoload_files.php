2b5073b2ad34b41d736258c7363919a16114a227
hasan jahjah <hasanjahjah224@gmail.com>
2019-12-08 09:18:40 +0200

    Revert "fix composer error."
    This reverts commit cc9878988cd1dd2e6d074450dd7a9ecd6fef4509.

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -15,7 +15,6 @@ return array(
     'bd9634f2d41831496de0d3dfe4c94881' => $vendorDir . '/symfony/polyfill-php56/bootstrap.php',
     '6124b4c8570aa390c21fafd04a26c69f' => $vendorDir . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
     'e7223560d890eab89cda23685e711e2c' => $vendorDir . '/psy/psysh/src/Psy/functions.php',
-    'e39a8b23c42d4e1452234d762b03835a' => $vendorDir . '/ramsey/uuid/src/functions.php',
     'f0906e6318348a765ffb6eb24e0d0938' => $vendorDir . '/laravel/framework/src/Illuminate/Foundation/helpers.php',
     '58571171fd5812e6e447dce228f52f4d' => $vendorDir . '/laravel/framework/src/Illuminate/Support/helpers.php',
 );
